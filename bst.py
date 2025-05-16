5b5b73f050caa368fad60e171353860fa4259851
DustenL <dulo@calpoly.edu>
2025-05-16 15:53:49 -0700

    starter

--- bst.py
+++ bst.py
@@ -4,6 +4,23 @@ from typing import *
 from dataclasses import dataclass
 sys.setrecursionlimit(10**6)
 
+BinTree: TypeAlias = Union["Node", None]
+
+@dataclass(frozen=True)
+class Node:
+    v: any
+    l: BinTree
+    r: BinTree
+
+@dataclass(frozen=True)
+class BinarySearchTree:
+    x: BinTree
+    y: Callable[]
+    #
+    # def comes_before(self):
+    #     if l < v:
+    #
+    #     v < r
 
 def example_fun(x : int) -> bool:
     return x < 142
