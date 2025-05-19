566235689e51d847b0758532a31512310562c610
bella940118 <ychu06@calpoly.edu>
2025-05-19 15:50:09 -0700

    Update bst.py

--- bst.py
+++ bst.py
@@ -14,14 +14,32 @@ class Node:
 
 @dataclass(frozen=True)
 class BinarySearchTree:
-    x: BinTree
-    y: Callable[[any, any], bool]
+    bt: BinTree
+    comes_before: Callable[[any, any], bool]
 
-def comes_before(x, y):
-    return x < y
-
-def example_fun(x : int) -> bool:
-    return x < 142
 
 def is_empty(bst: BinarySearchTree) -> bool:
     return bst.x is None
+
+
+def insert_helper(bt: BinTree, value: any, comes_before: Callable[[any, any], bool]) -> BinTree:
+    match bt:
+        case None:
+            return Node(value, None, None)
+        case Node(v, l, r):
+            if comes_before(value, v):
+                return Node(v, insert_helper(l, value, comes_before), r)
+            else:
+                return Node(v, l, insert_helper(r, value, comes_before))
+
+
+def insert(bst: BinarySearchTree, value: any) -> BinarySearchTree:
+    new_tree = insert_helper(bst.x, value, bst.y)
+    return BinarySearchTree(new_tree, bst.y)
+
+
+class TestBST(unittest.TestCase):
+    def test_insert(self):
+        tree = BinarySearchTree(None, comes_before)
+        tree = insert(tree, 10)
+        self.assertEqual(tree, BinarySearchTree)
