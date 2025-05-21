625a637fbef2f62fe06c7244956b0318594d6e02
bella940118 <ychu06@calpoly.edu>
2025-05-21 12:54:46 -0700

    Update bst_tests.py

--- bst_tests.py
+++ bst_tests.py
@@ -7,9 +7,23 @@ sys.setrecursionlimit(10**6)
 from bst import *
 
 class BSTTests(unittest.TestCase):
-    def test_example_fun(self):
-        self.assertEqual(True, example_fun(34))
-        self.assertEqual(False,example_fun(1423))
+
+    def test_is_empty(self):
+        tree = BinarySearchTree(None, compare)
+        check = is_empty(tree)
+        self.assertEqual(True, check)
+
+    def test_insert(self):
+        tree = BinarySearchTree(None, compare)
+        tree = insert(tree, 10)
+        expected = BinarySearchTree(Node(10, None, None),compare)
+        self.assertEqual(tree, expected)
+
+    def test_delete(self):
+        tree= BinarySearchTree(Node(5, Node(3, None, None), Node(7, None,None)), compare)
+        result = delete(tree, 3)
+        expected = BinarySearchTree(Node(5, None, Node(7, None, None)), compare)
+        self.assertEqual(result, expected)
 
 if (__name__ == '__main__'):
     unittest.main()
